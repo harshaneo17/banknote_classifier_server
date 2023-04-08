84d66479fc8e3464751f535a0bc7dab59f396dd2
harshaneo17 <harsha.god86@gmail.com>
2023-04-08 22:16:06 +0100

    tested the API on postman and few bug fixes

--- modelserver.py
+++ modelserver.py
@@ -1,21 +1,32 @@
+import pandas as pd
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
 # Create flask app
-flask_app = Flask(__name__)
+app = Flask(__name__)
 model = pickle.load(open("banknotesmodel.pkl", "rb"))
 
-@flask_app.route("/")
+@app.route("/")
 def Home():
     return render_template("index.html")
 
-@flask_app.route("/predict", methods = ["POST"])
+@app.route("/predict", methods = ["POST"])
+
+#For web application
 def predict():
     float_features = [float(x) for x in request.form.values()]
     features = [np.array(float_features)]
     prediction = model.predict(features)
     return render_template("index.html", prediction_text = f"The bank note is {prediction}")
+    
+
+#For POSTMAN TESTING API
+# def predict():
+    # json_ = request.json
+    # query_df = pd.DataFrame(json_)
+    # prediction = model.predict(query_df)
+    # return jsonify({"Prediction": prediction.tolist()})
 
 if __name__ == "__main__":
-    flask_app.run(debug=True)
+    app.run(debug=True)
